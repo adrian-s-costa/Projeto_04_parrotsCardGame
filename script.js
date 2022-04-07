687dfbafe43ded2986afcfc2cdfae4903a3e7984
Adrian <adriancosta1215@gmail.com>
2022-04-07 11:38:53 -0300

    Dar cartas dinamicamente - implementado

--- script.js
+++ script.js
@@ -1,5 +1,44 @@
 function virarCarta(elementoClicado){
     elementoClicado.querySelector(".carta-verso").classList.add("carta-vira")
+    elementoClicado.querySelector(".carta-verso").classList.add("carta-vira")
     elementoClicado.querySelector(".carta-vira").classList.remove("carta-verso")
     console.log(elementoClicado)
 }
+
+function darCartas(){
+    
+    
+    
+    let cartas = prompt("Com quantas cartas vc deseja jogar? (Entre 4 e 14)")
+
+    while (cartas < 4 || cartas > 14){
+        
+        console.log(cartas)
+
+        cartas = prompt("Valor inválido! Com quantas cartas você quer jogar? (Entre 4 e 14)")
+
+        
+
+    }
+
+    
+    const grupoCartas = document.querySelector(".cartas-agrupadas")
+    
+    grupoCartas.innerHTML = ""
+
+    for (let i = 0; i < cartas; i++){
+
+    grupoCartas.innerHTML += ` 
+        <div class="cartas-especificas" onclick="virarCarta(this)">
+            <div class="carta-frente face">
+                <img src="imagens/front.png" >
+            </div>
+            <div class="carta-verso face">
+                verso
+            </div>
+        </div>
+
+    `
+   }
+}
+
